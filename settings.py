b3fc60bded9bd220d81173da5d7ed2f2bfbb2cba
NolanAgah <N.Agah24@bcp.org>
2023-03-15 08:53:28 -0700

    test

---
+++ settings.py
@@ -0,0 +1,22 @@
+# file created by NOLAN AGAH
+
+# define colors
+BLACK = (0,0,0)
+BLUE = (50,50,255)
+RED = (255,50,50)
+
+# define size of window
+WIDTH = 800
+HEIGHT = 600
+
+# game settings:
+FPS = 30
+
+
+# define player attributes
+PLAYER_ACC = 2
+PLAYER_FRICTION = -0.12
+
+# define mob attributes
+MOB_ACC = 2
+MOB_FRICTION = -0.12
